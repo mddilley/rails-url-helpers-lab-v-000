eacf6c8ca076c1d16c552bbf27e3ee0b0491b77d
Mike Dilley <mike.dilley@gmail.com>
2019-01-10 16:51:07 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,7 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find(params[:id])
+    binding.pry
     @student.active == false ? @student.active = true : @student.active = false
     redirect_to student_path(@student)
   end
