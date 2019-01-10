14217ab4a0f0f7c6dc7c8a1cc50eb5177afbf8e3
Mike Dilley <mike.dilley@gmail.com>
2019-01-10 16:33:25 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,7 @@ class StudentsController < ApplicationController
 
   def activate
     set_student
+    @student.active == true ? @student.active = false : @student.active = true
   end
 
   private
